0dee0458583924eb93d71fb28630c574725d8ae7
David Lukele <d.lukele@testbirds.de>
2017-10-17 16:19:01 +0200

    First Setup

--- webdriverio-test/features/step_definitions/my_steps.js
+++
@@ -1,8 +0,0 @@
-import openWebsite from '../support/action/openWebsite';
-
-module.exports = function when() {
-    this.When(
-        /^I open the (url|site) "([^"]*)?"$/,
-        openWebsite
-    );
-};
